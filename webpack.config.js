18d079040cada2ad728b9c807af4eb8e02906b7c
amissah17 <amissahfrank17@gmail.com>
2023-05-11 03:10:54 +0000

    update: minor test updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode : 'development',
-    entry: path.resolve(__dirname,'src/scripts/index.js'),
-    output:{
-        path: path.resolve(__dirname,'dist'), 
-        filename: 'bundle.js',
-    } 
-
-}
+  mode: "development",
+  entry:{ bundle: path.resolve(__dirname, "src/scripts/index.js"),},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [ new HtmlWebpackPlugin({ filename: 'index.html',template: './src/index.html' }),]
+};
